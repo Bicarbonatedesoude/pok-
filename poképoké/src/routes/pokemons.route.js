e952a255f2b4c5418c7ed0e5ea26c853d37eb9c4
Bicarbonate Bicarbonate <allo>
2024-03-14 09:08:52 -0400

    modif

--- poképoké/src/routes/pokemons.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pokemonsController = require('../controllers/pokemons.controller');
-
-router.get('/:id', (req, res) => {
-    pokemonsController.trouverUnPokemon(req, res);
-});
-
-module.exports = router;
